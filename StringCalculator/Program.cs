c36b98bb3ad9b27d700381b6480acba5d7703c65
anthony bobbie <44766686+bowaybilly@users.noreply.github.com>
2021-06-09 08:10:13 +0100

    Update Program.cs

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -9,19 +9,11 @@ namespace StringCalculatorAssignment
         private static IKernel Kernel;
         static void Main(string[] args)
         {
-            Kernel = new StandardKernel(new Container());
-            var stringCalculator = Kernel.Get<StringCalculator>();
-            StartStringAdditions(stringCalculator);
+           
 
         }
 
-        public static void StartStringAdditions(StringCalculator stringCalculator)
-        {
-            var answer = stringCalculator.Add("");
-            Console.WriteLine($"zero length input returns {answer}");
-            Console.WriteLine($"1 add 2 returns {stringCalculator.Add("1,2")}");
-
-        }
+       
 
         
     }
